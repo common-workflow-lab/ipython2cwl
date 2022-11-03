57258d1e0c06ace7501a75b028fa93d2d77e7b4e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-03 16:29:51 +0000

    Adding tarfile member sanitization to extractall()

--- tests/test_cwltoolextractor.py
+++ tests/test_cwltoolextractor.py
@@ -84,7 +84,26 @@ class TestCWLTool(TestCase):
               AnnotatedIPython2CWLToolConverter(annotated_python_script)
               .compile(Path(compiled_tar_file)))
         with tarfile.open(compiled_tar_file, 'r') as tar:
-            tar.extractall(path=extracted_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=extracted_dir)
         print(compiled_tar_file)
         self.assertSetEqual(
             {'notebookTool', 'tool.cwl', 'Dockerfile', 'requirements.txt', 'setup.py'},
